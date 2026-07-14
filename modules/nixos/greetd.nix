e0c3e42d354f264603cae8d6be8a17e968240e93
ecomex <skoch@sks-concept.de>
2026-07-14 17:25:34 +0200

    greetd: fix tuigreet --sessions flag (was treated as directory path)

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions niri";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
       };
     };
   };
