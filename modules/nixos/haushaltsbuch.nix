77d0d09839c82ed678315b8ef43604e1d104b231
ecomex <skoch@sks-concept.de>
2026-08-03 20:35:43 +0200

    haushaltsbuch: pin image 1fcfa9e
    ghcr.io/ec0m3x/haushaltsbuch@sha256:8295f0b88613c0f512ef753b140129359db5e661b281a1dcc0f9da4c26d3f769
    
    Built by ci-image.yml from ec0m3x/haushaltsbuch@1fcfa9eb63c6fc306fc5dd614a1d60d73f2928cd.

--- modules/nixos/haushaltsbuch.nix
+++ modules/nixos/haushaltsbuch.nix
@@ -11,8 +11,8 @@
 #   2. Digest hier eintragen, committen, pushen.
 #   3. GitOps-Controller auf nix-ai rollt hl01 aus.
 {config, ...}: let
-  # Tag ae59268 (App-Repo main, 2026-08-03).
-  haushaltsbuchImage = "ghcr.io/ec0m3x/haushaltsbuch@sha256:45414a9db5c23c22d2c40f6d892e368279e46b661ce2fab7b3c859f28816feee";
+  # Tag 1fcfa9e (App-Repo main, 2026-08-03).
+  haushaltsbuchImage = "ghcr.io/ec0m3x/haushaltsbuch@sha256:8295f0b88613c0f512ef753b140129359db5e661b281a1dcc0f9da4c26d3f769";
   ghcrLogin = {
     registry = "ghcr.io";
     username = "ec0m3x";
